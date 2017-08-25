8d5760a5157dd3566026b27725c05371271a8b2c
user <user@user-PC>
2017-08-25 14:18:17 +0900

    수정완료 했습니다.

--- PetManagementProgram/src/InfoDAO.java
+++ PetManagementProgram/src/InfoDAO.java
@@ -43,5 +43,41 @@ public class InfoDAO {//애견정보&보호자 입력 테이블
 		}
 
 	}
+	public void insertPetInfo(int regiNum, String name, int gender, String species, String form, int age, int weight, String comments) {
+		//펫 정보를 DB에 Insert 하기
+				if (regiNum == 0) {
+				} else {
+					try { // 예외처리 할 부분
+
+						Class.forName("oracle.jdbc.driver.OracleDriver");
+						conn = DriverManager.getConnection(url, id, pw);
+						String sql = "insert into login(regiNum,name,gender,species,form,age,weight,comments)" + "values(?,?,?,?,?,?,?,?)";
+						pstmt = conn.prepareStatement(sql);
+						pstmt.setInt(1, regiNum);
+						pstmt.setString(2, name);
+						pstmt.setInt(3, gender);
+						pstmt.setString(4, species);
+						pstmt.setString(5, form);
+						pstmt.setInt(6, age);
+						pstmt.setInt(7, weight);
+						pstmt.setString(8, comments);
+						pstmt.executeUpdate();
+					} catch (ClassNotFoundException e) { // 예외처리 잡아주는 부분
+						e.printStackTrace(); // 예외에 대한 자세한 사항을 consol창에 출력
+					} catch (SQLException e) {// 아이디중복처리
+						System.out.println("아이디중복");
+					} finally { // finally 블록은 try에서 예외가 발생하든 안하든 무조건 실행됨.
+					}
+					try {
+						if (pstmt != null)
+							pstmt.close(); // 닫는부분도 앞의 상황에 따라 수정.
+						if (conn != null)
+							conn.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+
+			}
 
 }
--- PetManagementProgram/src/LoginGUI.java
+++ PetManagementProgram/src/LoginGUI.java
@@ -147,6 +147,24 @@ public class LoginGUI {
 		panel.add(newMember);
 		
 		JButton login = new JButton("\uB85C\uADF8\uC778");
+		login.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				LoginDAO login1 = new LoginDAO(); //loginDAO 객체 생성
+				LoginVO vo=login1.selectLogin(idInput.getText(), pwInput.getText());//loginDAO에 관리자 인지아닌지 구분하는 메소드 selectLogin구현
+				//LoginVO객체를 생성해서 m값을 받음
+				if(vo.getM()==1){
+					MenuGUI manager = new MenuGUI();
+					manager.main(null);
+					frame.dispose();
+				}
+				else{
+					MenuGUI general = new MenuGUI();
+					general.main(null);
+					frame.dispose();
+				}
+			}
+		});
 		springLayout.putConstraint(SpringLayout.NORTH, panel, -278, SpringLayout.SOUTH, login);
 		login.setBounds(140, 350, 110, 30);
 		panel.add(login);
