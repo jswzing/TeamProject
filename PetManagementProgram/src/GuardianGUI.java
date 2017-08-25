048123aeea72cb3fa9816933c09f8638c133d888
user <user@user-PC>
2017-08-25 10:47:49 +0900

    Guardian GUI - Frame size fixed

--- PetManagementProgram/src/GuardianGUI.java
+++ PetManagementProgram/src/GuardianGUI.java
@@ -13,6 +13,8 @@ import java.awt.SystemColor;
 import java.awt.Color;
 import java.awt.Button;
 import javax.swing.JTextArea;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.BevelBorder;
 
 public class GuardianGUI {
 
@@ -41,103 +43,99 @@ public class GuardianGUI {
 		initialize();
 	}
 
-	/**S
+	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
 		frame = new JFrame();
 		frame.setResizable(false);
-		frame.setBounds(100, 100, 700, 600);
+		frame.setBounds(100, 100, 741, 613);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JPanel panel_8 = new JPanel();
-		panel_8.setBounds(0, 0, 707, 575);
-		frame.getContentPane().add(panel_8);
-		
-		JLabel lblNewLabel = new JLabel("\uBCF4\uD638\uC790 \uC815\uBCF4");
-		frame.getContentPane().add(lblNewLabel);
-		lblNewLabel.setBackground(new Color(153, 180, 209));
-		lblNewLabel.setLabelFor(frame);
-		lblNewLabel.setFont(new Font("±¼¸²", Font.PLAIN, 50));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(147, 171, 94, 28);
-		frame.getContentPane().add(panel_1);
-		panel_1.setLayout(new CardLayout(0, 0));
-		
-		JLabel lblNewLabel_1 = new JLabel("\uC774\uB984");
-		lblNewLabel_1.setFont(new Font("±¼¸²", Font.BOLD, 18));
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
-		panel_1.add(lblNewLabel_1, "name_10410950743880");
-		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(147, 217, 94, 22);
-		frame.getContentPane().add(panel_2);
-		panel_2.setLayout(new CardLayout(0, 0));
-		
-		JLabel lblNewLabel_2 = new JLabel("\uC5F0\uB77D\uCC98");
-		lblNewLabel_2.setFont(new Font("±¼¸²", Font.BOLD, 18));
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.LEFT);
-		panel_2.add(lblNewLabel_2, "name_10427619988567");
-		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBounds(147, 261, 94, 22);
-		frame.getContentPane().add(panel_3);
-		panel_3.setLayout(new CardLayout(0, 0));
-		
-		JLabel lblNewLabel_3 = new JLabel("\uC8FC\uC18C");
-		lblNewLabel_3.setFont(new Font("±¼¸²", Font.BOLD, 18));
-		lblNewLabel_3.setHorizontalAlignment(SwingConstants.LEFT);
-		panel_3.add(lblNewLabel_3, "name_10445331306559");
-		
-		JPanel panel_4 = new JPanel();
-		panel_4.setBounds(261, 171, 269, 28);
-		frame.getContentPane().add(panel_4);
-		panel_4.setLayout(new CardLayout(0, 0));
-		
-		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setFont(new Font("Monospaced", Font.PLAIN, 15));
-		panel_4.add(textArea_1, "name_9502280390988");
-		
-		JPanel panel_5 = new JPanel();
-		panel_5.setBounds(261, 217, 269, 28);
-		frame.getContentPane().add(panel_5);
-		panel_5.setLayout(new CardLayout(0, 0));
-		
-		JTextArea textArea_2 = new JTextArea();
-		textArea_2.setFont(new Font("Monospaced", Font.PLAIN, 15));
-		panel_5.add(textArea_2, "name_9505344412199");
-		
-		JPanel panel_6 = new JPanel();
-		panel_6.setBounds(261, 261, 269, 50);
-		frame.getContentPane().add(panel_6);
-		panel_6.setLayout(new CardLayout(0, 0));
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setFont(new Font("Monospaced", Font.PLAIN, 15));
-		panel_6.add(textArea, "name_9493520574929");
+		JPanel background = new JPanel();
+		background.setBorder(new CompoundBorder());
+		background.setBounds(10, 10, 712, 564);
+		frame.getContentPane().add(background);
+		background.setLayout(null);
 		
 		JPanel panel_7 = new JPanel();
-		panel_7.setBounds(418, 367, 112, 28);
-		frame.getContentPane().add(panel_7);
+		panel_7.setBounds(430, 444, 108, 34);
+		background.add(panel_7);
 		panel_7.setLayout(new CardLayout(0, 0));
 		
-		Button button = new Button("\uC644\uB8CC");
-		button.setFont(new Font("Dialog", Font.PLAIN, 17));
-		panel_7.add(button, "name_10848459115272");
-		
-		JLabel lblNewLabel_4 = new JLabel("\uC644\uB8CC");
-		lblNewLabel_4.setLabelFor(lblNewLabel_4);
-		lblNewLabel_4.setForeground(new Color(0, 0, 0));
-		lblNewLabel_4.setFont(new Font("±¼¸²", Font.BOLD, 18));
-		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_7.add(lblNewLabel_4, "name_10669186233266");
+		Button compButton = new Button("\uC644\uB8CC");
+		panel_7.add(compButton, "name_5037528703030");
+		compButton.setFont(new Font("Dialog", Font.PLAIN, 17));
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(147, 31, 383, 73);
-		frame.getContentPane().add(panel);
+		panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel.setBounds(140, 49, 398, 73);
+		background.add(panel);
 		panel.setLayout(new CardLayout(0, 0));
+		
+		JLabel guardianInfo = new JLabel("\uBCF4\uD638\uC790 \uC815\uBCF4");
+		panel.add(guardianInfo, "name_4988617370205");
+		guardianInfo.setBackground(SystemColor.window);
+		guardianInfo.setLabelFor(frame);
+		guardianInfo.setFont(new Font("±¼¸²", Font.PLAIN, 50));
+		guardianInfo.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JPanel panel_1_Name = new JPanel();
+		panel_1_Name.setBounds(140, 190, 94, 28);
+		background.add(panel_1_Name);
+		panel_1_Name.setLayout(new CardLayout(0, 0));
+		
+		JLabel Name = new JLabel("\uC774\uB984");
+		panel_1_Name.add(Name, "name_5382253871486");
+		Name.setFont(new Font("±¼¸²", Font.BOLD, 18));
+		Name.setHorizontalAlignment(SwingConstants.LEFT);
+		
+		JPanel panel_2_contact = new JPanel();
+		panel_2_contact.setBounds(140, 252, 94, 22);
+		background.add(panel_2_contact);
+		panel_2_contact.setLayout(new CardLayout(0, 0));
+		
+		JLabel contactNo = new JLabel("\uC5F0\uB77D\uCC98");
+		panel_2_contact.add(contactNo, "name_5004449119981");
+		contactNo.setFont(new Font("±¼¸²", Font.BOLD, 18));
+		contactNo.setHorizontalAlignment(SwingConstants.LEFT);
+		
+		JPanel panel_3_address = new JPanel();
+		panel_3_address.setBounds(140, 318, 94, 22);
+		background.add(panel_3_address);
+		panel_3_address.setLayout(new CardLayout(0, 0));
+		
+		JLabel address = new JLabel("\uC8FC\uC18C");
+		panel_3_address.add(address, "name_5008952993544");
+		address.setFont(new Font("±¼¸²", Font.BOLD, 18));
+		address.setHorizontalAlignment(SwingConstants.LEFT);
+		
+		JPanel panel_4_nameInput = new JPanel();
+		panel_4_nameInput.setBounds(269, 190, 269, 28);
+		background.add(panel_4_nameInput);
+		panel_4_nameInput.setLayout(new CardLayout(0, 0));
+		
+		JTextArea nameInput = new JTextArea();
+		panel_4_nameInput.add(nameInput, "name_5017744949718");
+		nameInput.setFont(new Font("Monospaced", Font.PLAIN, 15));
+		
+		JPanel panel_5_coontactInput = new JPanel();
+		panel_5_coontactInput.setBounds(269, 246, 269, 28);
+		background.add(panel_5_coontactInput);
+		panel_5_coontactInput.setLayout(new CardLayout(0, 0));
+		
+		JTextArea contactNoInput = new JTextArea();
+		panel_5_coontactInput.add(contactNoInput, "name_5024528764197");
+		contactNoInput.setFont(new Font("Monospaced", Font.PLAIN, 15));
+		
+		JPanel panel_6_addInput = new JPanel();
+		panel_6_addInput.setBounds(269, 318, 269, 78);
+		background.add(panel_6_addInput);
+		panel_6_addInput.setLayout(new CardLayout(0, 0));
+		
+		JTextArea addressInput = new JTextArea();
+		panel_6_addInput.add(addressInput, "name_5030856650982");
+		addressInput.setFont(new Font("Monospaced", Font.PLAIN, 15));
 	}
 }
